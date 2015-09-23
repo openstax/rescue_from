9ef054044d9cee4767c243caa03da6a93d878ee3
Joe Sak <joe@joesak.com>
2015-09-23 15:48:18 -0400

    Fix require in spec dummy

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -9,7 +9,7 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
-require "openstax_rescue_from"
+require "rescue_from"
 
 module Dummy
   class Application < Rails::Application
