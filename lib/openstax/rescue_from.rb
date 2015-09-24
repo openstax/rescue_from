88409018d0bacab2cd64444af2b90993d11bae84
Joe Sak <joe@joesak.com>
2015-09-24 15:11:17 -0400

    Make RescueFrom.perform_rescue the single point of contact for the command procedure

--- lib/openstax/rescue_from.rb
+++ lib/openstax/rescue_from.rb
@@ -8,12 +8,54 @@ require 'openstax/rescue_from/controller'
 module OpenStax
   module RescueFrom
     class << self
+      def perform_rescue(exception:, listener: MuteListener.new)
+        proxy = ExceptionProxy.new(exception)
+
+        listener.before_openstax_exception_rescue(proxy)
+        log_system_error(proxy)
+        send_notifying_exceptions(proxy)
+        finish_exception_rescue(proxy, listener)
+      end
+
       def configure
         yield configuration
       end
 
       def configuration
         @configuration ||= Configuration.new
+      end
+
+      private
+      def log_system_error(proxy)
+        logger = Logger.new(proxy)
+        logger.record_system_error!
+      end
+
+      def send_notifying_exceptions(proxy)
+        if proxy.notify?
+          configuration.notifier.notify_exception(
+            proxy.exception,
+            env: request.env,
+            data: {
+              error_id: proxy.error_id,
+              :class => proxy.name,
+              message: proxy.message,
+              first_line_of_backtrace: proxy.first_backtrace_line,
+              cause: proxy.cause,
+              dns_name: proxy.dns_name,
+              extras: proxy.extras
+            },
+            sections: %w(data request session environment backtrace)
+          )
+        end
+      end
+
+      def finish_exception_rescue(proxy, listener)
+        if configuration.raise_exceptions
+          raise proxy.exception
+        else
+          listener.after_openstax_exception_rescue(proxy)
+        end
       end
     end
   end
--- lib/openstax/rescue_from/controller.rb
+++ lib/openstax/rescue_from/controller.rb
@@ -7,56 +7,16 @@ module OpenStax
 
       module ClassMethods
         def use_openstax_exception_rescue
-          rescue_from Exception, with: :openstax_exception_rescue
+          rescue_from Exception, with: RescueFrom.perform_rescue(exception: exception,
+                                                                 listener: self)
         end
       end
 
       private
-      def openstax_exception_rescue(exception)
-        proxy = ExceptionProxy.new(exception)
-
-        before_openstax_exception_rescue(proxy)
-        log_system_error(proxy)
-        send_notifying_exceptions(proxy)
-        finish_exception_rescue(proxy)
-      end
-
       def before_openstax_exception_rescue(proxy)
         @message = proxy.friendly_message
         @code = proxy.status_code
         @error_id = proxy.error_id
-      end
-
-      def log_system_error(proxy)
-        logger = Logger.new(proxy)
-        logger.record_system_error!
-      end
-
-      def send_notifying_exceptions(proxy)
-        if proxy.notify?
-          openstax_rescue_config.notifier.notify_exception(
-            proxy.exception,
-            env: request.env,
-            data: {
-              error_id: proxy.error_id,
-              :class => proxy.name,
-              message: proxy.message,
-              first_line_of_backtrace: proxy.first_backtrace_line,
-              cause: proxy.cause,
-              dns_name: proxy.dns_name,
-              extras: proxy.extras
-            },
-            sections: %w(data request session environment backtrace)
-          )
-        end
-      end
-
-      def finish_exception_rescue(proxy)
-        if openstax_rescue_config.raise_exceptions
-          raise proxy.exception
-        else
-          after_openstax_exception_rescue(proxy)
-        end
       end
 
       def after_openstax_exception_rescue(proxy)
