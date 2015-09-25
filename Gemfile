dbc9d96e6284becceaae593a7d72551ddaf78338
Joe Sak <joe@joesak.com>
2015-09-25 13:46:21 -0400

    Setup spec dummy to handle manual browser testing

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in rescue_from.gemspec
 gemspec
+
+gem 'dotenv-rails'
--- spec/dummy/config/initializers/rescue_from.rb
+++ spec/dummy/config/initializers/rescue_from.rb
@@ -1,10 +1,10 @@
 require 'openstax_rescue_from'
 
 OpenStax::RescueFrom.configure do |config|
-  config.raise_exceptions = Rails.application.config.consider_all_requests_local
+  config.raise_exceptions = ![false, 'false'].include?(ENV['RAISE_EXCEPTIONS'])
 
   # config.app_name = 'Tutor'
-  # config.app_env = ENV['APP_ENV'] || 'DEV'
+  # config.app_env = ENV['APP_ENV']
 
   # config.logger = Rails.logger
   # config.notifier = ExceptionNotifier
@@ -12,9 +12,7 @@ OpenStax::RescueFrom.configure do |config|
   # config.html_error_template_path = 'errors/any'
   # config.html_error_template_layout_name = 'application'
 
-  # config.email_prefix = "[#{app_name}] (#{app_env}) "
-  # config.sender_address = ENV['EXCEPTION_SENDER'] ||
-  #                           %{"OpenStax Tutor" <noreply@openstax.org>}
-  # config.exception_recipients = ENV['EXCEPTION_RECIPIENTS'] ||
-  #                                 %w{tutor-notifications@openstax.org}
+  # config.email_prefix = app_name.blank? ? "" : "[#{app_name}] (#{app_env}) "
+  # config.sender_address = ENV['EXCEPTION_SENDER']
+  # config.exception_recipients = ENV['EXCEPTION_RECIPIENTS']
 end
