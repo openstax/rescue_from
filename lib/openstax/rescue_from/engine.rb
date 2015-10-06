811e04a6b9a3c5cfa6ed192e8ec6889a53af6a74
Joe Sak <joe@joesak.com>
2015-10-06 14:09:57 -0400

    Move inclusion of controller module out of initializer

--- lib/openstax/rescue_from/engine.rb
+++ lib/openstax/rescue_from/engine.rb
@@ -3,14 +3,12 @@ require 'openstax/rescue_from/default_exceptions'
 module OpenStax
   module RescueFrom
     class Engine < ::Rails::Engine
+      ActionController::Base.send :include, Controller
+
       initializer 'openstax.rescue_from.inflection' do
         ActiveSupport::Inflector.inflections do |inflect|
           inflect.acronym 'OpenStax'
         end
-      end
-
-      initializer "openstax.rescue_from.action_controller" do
-        ActionController::Base.send :include, Controller
       end
 
       initializer "openstax.rescue_from.view_helpers" do
